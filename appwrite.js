12c386d904c103ff4fec0b4a53866d0d22195027
Ritik._.Thakur <greatritik234@gmail.com>
2025-11-18 01:26:32 +0530

    Update appwrite.js

--- appwrite.js
+++ appwrite.js
@@ -1,1 +1,8 @@
 
+const client = new Appwrite.Client();
+
+client
+  .setEndpoint("https://fra.cloud.appwrite.io/v1")  // Appwrite API endpoint
+  .setProject("691b7ab500093cb35c48");              // Your Appwrite Project I
+
+const databases = new Appwrite.Databases(client);
